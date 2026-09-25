644394fe558505f96bb78bef13fc5c71a99e069c
Yutaka Imagawa <flugel.biz@gmail.com>
2026-09-26 00:31:58 +0900

    Meta 機構に非存在フォルダのエラー表示処理追加

--- app/components/routes/_models/siteMeta.ts
+++ app/components/routes/_models/siteMeta.ts
@@ -2,6 +2,7 @@ import { buildMeta } from "@/_global/lib/meta";
 import type { YamlFiles, MetaProps } from "@/_global/lib/meta";
 
 const globData = import.meta.glob('../**/_data/meta.yaml', { eager: true }) as YamlFiles;
+const pageData = import.meta.glob('../**/Page.vue', { eager: true });
 export const buildSiteMeta = (props?: MetaProps) => {
-	return buildMeta({ ...props ?? {}, globData })
+	return buildMeta({ ...props ?? {}, globData, pageData })
 };
